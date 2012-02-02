306ad14449f6a26f8f854721d66a13a5e71d5cb1
Thomas Becker <tbecker@intalio.com>
2012-02-02 19:40:32 +0100

    commandline argument changes: install <pluginname>, list and JETTY_HOME env is read now

--- jetty-plugins/src/main/java/org/eclipse/jetty/plugins/Main.java
+++ jetty-plugins/src/main/java/org/eclipse/jetty/plugins/Main.java
@@ -14,6 +14,7 @@
 package org.eclipse.jetty.plugins;
 
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jetty.plugins.aether.AetherService;
 import org.eclipse.jetty.plugins.aether.impl.AetherServiceImpl;
@@ -22,69 +23,75 @@ import org.eclipse.jetty.plugins.impl.PluginManagerImpl;
 /* ------------------------------------------------------------ */
 /**
  */
-public class Main
-{
-    private AetherService _aetherService = new AetherServiceImpl();
-    private PluginManager _pluginManager;
-    private String _jettyHome;
-    private String _installPlugin;
-    private boolean _listPlugins;
+public class Main {
+	private static final String JETTY_HOME = "JETTY_HOME";
 
-    /* ------------------------------------------------------------ */
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        Main main = new Main();
-        main.execute(args);
-    }
+	private AetherService _aetherService = new AetherServiceImpl();
+	private PluginManager _pluginManager;
+	private String _jettyHome;
+	private String _installPlugin;
+	private boolean _listPlugins;
 
-    private void execute(String[] args)
-    {
-        parseCommandline(args);
+	/* ------------------------------------------------------------ */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.execute(args);
+	}
 
-        _pluginManager = new PluginManagerImpl(_aetherService,_jettyHome);
+	private void execute(String[] args) {
+		parseEnvironmentVariables();
+		parseCommandline(args);
 
-        if (_listPlugins)
-            listPlugins();
+		_pluginManager = new PluginManagerImpl(_aetherService, _jettyHome);
 
-        if (_installPlugin != null)
-            installPlugin();
-    }
+		if (_listPlugins)
+			listPlugins();
+		else if (_installPlugin != null)
+			installPlugin();
+	}
 
-    private void listPlugins()
-    {
-        List<String> availablePlugins = _pluginManager.listAvailablePlugins();
-        for (String plugin : availablePlugins)
-            System.out.println(plugin);
-    }
+	private void listPlugins() {
+		List<String> availablePlugins = _pluginManager.listAvailablePlugins();
+		for (String plugin : availablePlugins)
+			System.out.println(plugin);
+	}
 
-    private void installPlugin()
-    {
-        _pluginManager.installPlugin(_installPlugin);
-        System.out.println("Successfully installed plugin: " + _installPlugin);
-    }
+	private void installPlugin() {
+		_pluginManager.installPlugin(_installPlugin);
+		System.out.println("Successfully installed plugin: " + _installPlugin + " to " + _jettyHome);
+	}
 
-    private void parseCommandline(String[] args)
-    {
-        for (String arg : args)
-        {
-            if (arg.startsWith("--jettyHome="))
-                _jettyHome = arg.substring(12);
-            if (arg.startsWith("--installPlugin="))
-                _installPlugin = arg.substring(16);
-            if ("--listPlugins".equals(arg))
-                _listPlugins = true;
-        }
-        
-        // TODO: Usage instead of throwing exceptions
-        if (_jettyHome == null && _installPlugin != null)
-            throw new IllegalArgumentException("No --jettyHome commandline option specified!");
-        if (_installPlugin == null && _listPlugins == false)
-            throw new IllegalArgumentException("Neither --installPlugin=<pluginname> nor --listPlugins commandline option specified. Nothing to do for me!");
-        if (_installPlugin != null && _listPlugins)
-            throw new IllegalArgumentException(
-                    "Please specify either --installPlugin=<pluginname> or --listPlugins commandline options, but not both at the same time!");
-    }
+	private void parseEnvironmentVariables() {
+		Map<String, String> env = System.getenv();
+		if (env.containsKey(JETTY_HOME)) {
+			_jettyHome = env.get(JETTY_HOME);
+		}
+	}
+
+	private void parseCommandline(String[] args) {
+		int i = 0;
+		for (String arg : args) {
+			i++;
+			if (arg.startsWith("--jettyHome="))
+				_jettyHome = arg.substring(12);
+			if (arg.startsWith("install"))
+				_installPlugin = args[i];
+			if ("list".equals(arg))
+				_listPlugins = true;
+		}
+
+		// TODO: Usage instead of throwing exceptions
+		if (_jettyHome == null && _installPlugin != null)
+			throw new IllegalArgumentException(
+					"No --jettyHome commandline option specified and no \"JETTY_HOME\" environment variable found!");
+		if (_installPlugin == null && _listPlugins == false)
+			throw new IllegalArgumentException(
+					"Neither install <pluginname> nor list commandline option specified. Nothing to do for me!");
+		if (_installPlugin != null && _listPlugins)
+			throw new IllegalArgumentException(
+					"Please specify either install <pluginname> or list commandline options, but not both at the same time!");
+	}
 }
